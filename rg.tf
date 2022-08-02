500cd1a1d7d6306ec5688c475ce1851190fb38d2
kuchalakanti <kuchalakanti@gmail.com>
2022-08-02 09:23:25 +0530

    added Vnet code

--- rg.tf
+++ rg.tf
@@ -7,3 +7,10 @@ resource "azurerm_resource_group" "example1" {
   name     = var.rg1_name
   location = var.loc-name
 }
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+}
