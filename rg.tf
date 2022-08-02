ca28c420b8a80edd61127b5188e106058aaffe8c
kuchalakanti <kuchalakanti@gmail.com>
2022-08-02 09:20:52 +0530

    added another rg

--- rg.tf
+++ rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example" {
   name     = var.rg_name
   location = var.loc-name
 }
+
+resource "azurerm_resource_group" "example1" {
+  name     = var.rg1_name
+  location = var.loc-name
+}
--- variable.tf
+++ variable.tf
@@ -1,11 +1,16 @@
 variable "rg_name" {
     type = string
     
-  
 }
 
 variable "loc-name" {
     type = string
     
   
+}
+
+
+variable "rg1_name" {
+    type = string
+    
 }
